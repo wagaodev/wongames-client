e3de2390f2f3c4cf509bea60ddceb6dac465f61f
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2021-09-12 09:26:41 -0300

    feat: add component gamecard

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react';
+import GameCard, { GameCardProps } from '.';
+
+export default {
+  title: 'Components/GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+  },
+} as Meta;
+
+const Template: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
