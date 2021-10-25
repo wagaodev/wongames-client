f6d17bcd0216549934b8bbf35675b2f9a5ff0135
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2021-10-25 07:46:12 -0300

    refactor: improve home tests mocking components

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/mock.ts',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.d.ts',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
 };
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -21,36 +21,49 @@ const props = {
   freeHighligth: highlightMock,
 };
 
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Menu"></div>;
+    },
+  };
+});
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Footer"></div>;
+    },
+  };
+});
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Showcase"></div>;
+    },
+  };
+});
+
+jest.mock('components/BannerSlider', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Banner Slider"></div>;
+    },
+  };
+});
+
 describe('Component: <Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />);
 
-    // Menu
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
-
-    // Logos (menu/footer)
-    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
-
-    // Banner
-    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1);
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(5);
-    expect(screen.getAllByText(/borderlands is back!/i)).toHaveLength(3);
-
-    // Sections
-    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument();
-    expect(
-      screen.getByRole('heading', { name: /most popular/i })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole('heading', { name: /upcomming/i })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole('heading', { name: /free games/i })
-    ).toBeInTheDocument();
-
-    // Footer
-    expect(
-      screen.getByRole('heading', { name: /follow us/i })
-    ).toBeInTheDocument();
+    expect(screen.getByTestId('Mock Menu')).toBeInTheDocument();
+    expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument();
+    expect(screen.getByTestId('Mock Footer')).toBeInTheDocument();
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5);
   });
 });
